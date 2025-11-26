2168ef731007846bb6bb1372be5c4782330dd743
Bogusław Nozdryń <118533524+BoguNoz@users.noreply.github.com>
2025-11-26 17:20:47 +0100

    SIM-0005: Adding new partail composite feature

--- src/models/base-composite-model.ts
+++ src/models/base-composite-model.ts
@@ -12,6 +12,36 @@ export default interface BaseCompositeModel {
     id: string;
 
     /**
+     * Indicates whether the composite is a *partial composite*.
+     *
+     * @remarks
+     * -  **Partial composite** is initialized, and store registered, by parent composite.
+     * - A **partial composite** is a specialized composite that exists as a
+     *   fragment of a larger parent composite.
+     * - Partial composites are used to break down complex UI structures into smaller,
+     *   reusable segments.
+     * - Partial composites **cannot contain child composites**. They may contain only fields.
+     * - Typically used for grouping logical parts of a form without representing a standalone component.
+     */
+    isPartial: boolean;
+
+    /**
+     * A list of *partial composites* that belong to this composite.
+     *
+     * @remarks
+     * - Partials are smaller, reusable composite fragments that form sections
+     *   inside a larger composite.
+     * - Unlike full composites, partials **cannot contain their own child composites** —
+     *   they can only contain fields.
+     * - This property allows composing complex UIs by nesting predefined partial structures
+     *   without duplicating configuration logic.
+     * - Partials inherit the parent composite's lifecycle, including initialization and teardown.
+     *
+     * @see isPartial
+     */
+    partials: BaseCompositeModel[];
+
+    /**
      * A list of field configurations that belong to this composite.
      *
      * @remarks
