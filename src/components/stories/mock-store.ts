d7176e7df8ab1d472e1376dafd84e42e80fc6fcb
bnozdryn <b.nozdryn@gmail.com>
2026-04-27 13:56:29 +0200

    ENG-0004: Add fix to package build problem

--- src/components/stories/mock-store.ts
+++ src/components/stories/mock-store.ts
@@ -1,7 +1,7 @@
 import { BaseStore } from "@core/stores/base-store";
-import BaseFieldModel from "@core/models/base-field-model";
 import { BaseOperationFn } from "@core/events/operation";
 import {autoRegister} from "@core/engine/registres/auto-register";
+import { BaseFieldModel } from "@core/models/base-field-model";
 
 class MockStore extends BaseStore {
     override fields: Record<string, BaseFieldModel> = {};
