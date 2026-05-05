9dfeffcb7565f51b29fbcc9a530e4cd561402639
bnozdryn <b.nozdryn@gmail.com>
2026-05-05 12:34:26 +0200

    MAIN: Fix to export error

--- src/components/index.ts
+++ src/components/index.ts
@@ -20,3 +20,33 @@ export * from "./layout/partials/BaseToggle";
 // Layout
 export * from "./layout/BaseField";
 export * from "./layout/FormField";
+
+// UI
+export * from "./ui/accordion";
+export * from "./ui/alert";
+export * from "./ui/aspect-ratio";
+export * from "./ui/badge";
+export * from "./ui/button";
+export * from "./ui/button-group";
+export * from "./ui/card";
+export * from "./ui/carousel";
+export * from "./ui/checkbox";
+export * from "./ui/collapsible";
+export * from "./ui/dialog";
+export * from "./ui/form";
+export * from "./ui/input";
+export * from "./ui/input-otp";
+export * from "./ui/label";
+export * from "./ui/responsive";
+export * from "./ui/scroll-area";
+export * from "./ui/select";
+export * from "./ui/separator";
+export * from "./ui/sheet";
+export * from "./ui/sidebar";
+export * from "./ui/skeleton";
+export * from "./ui/sonner";
+export * from "./ui/spiner";
+export * from "./ui/switch";
+export * from "./ui/tabs";
+export * from "./ui/toggle";
+export * from "./ui/tooltip";
